e926f2890255eed68c95a2128767087d336a5ddb
thahir <thahirakram289@gmail.com>
2018-09-17 18:27:22 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ModelsComponent } from './models/models.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ModelsComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
